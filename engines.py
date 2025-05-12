bb85af02f867de119c012872364b8146dd1df0d0
Admin <assemstudy86@gmail.com>
2025-05-12 16:07:40 +0300

    final commit

--- engines.py
+++
@@ -1,6 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from config import DB_URIS
-
-engines = {name: create_engine(uri) for name, uri in DB_URIS.items()}
-Sessions = {name: sessionmaker(bind=engine) for name, engine in engines.items()}
