b964a105efb87718dca0b6d8ad6400975dd02bd0
Admin <assemstudy86@gmail.com>
2025-05-13 03:14:16 +0300

    final commit

--- config.py
+++ config.py
@@ -1,10 +1,3 @@
-# DB_URIS = {
-#     "user_db": "mysql+pymysql://root:@127.0.0.1/user_db",
-#     "payments_db": "mysql+pymysql://root:@127.0.0.1/payments_db",
-#     "debt_db": "mysql+pymysql://root:@127.0.0.1/debt_db",
-#     "history_db": "mysql+pymysql://root:@127.0.0.1/history_db",
-#     "mix_db": "mysql+pymysql://root:@127.0.0.1/mix_db",
-# }
 DB_CONFIG = {
     'user': 'root',
     'password': '',
